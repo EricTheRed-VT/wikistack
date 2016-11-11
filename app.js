4cdf00aa6bab502fbe68b5600630e245c62bf84f
Eric <ericrainvillevt@gmail.com>
2016-11-11 17:35:13 -0500

    fixed?

---
+++ app.js
@@ -0,0 +1,32 @@
+'use strict';
+//setup requires
+var express = require('express');
+var morgan = require('morgan');
+var nunjucks = require('nunjucks');
+var makesRouter = require('./routes');
+var fs = require('fs');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+//setup express and routes
+var app = express();
+app.use('/', routes);
+app.use(express.static('public'))
+app.use(express.static('views'))
+
+//setup port
+const port = 3000;
+app.listen(3000);
+
+//setup nunjucks
+var env = nunjucks.configure('views', {noCache: true});
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+
+//setup bodyparser
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.json())
+
+
+
+
